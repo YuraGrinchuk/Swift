76dc1238d4aa912729342d5f9835cddc8db98b16
Yurii Hrynchuk <yura.04056@gmail.com>
2018-12-27 20:46:06 -0500

    functions

--- TestApp.playground/Contents.swift
+++ TestApp.playground/Contents.swift
@@ -1,11 +1,22 @@
 import UIKit
-
-var a:Int = 11
-var b:Int = 3
-
-if a%b==0{
-    print("a divided on b")
+//---------------------------------------------------------------------------------------------------------------
+// function way to find a max from three numbers
+func maxFinder(IntValue numb1:Int, IntValue numb2:Int, IntValue numb3:Int) -> Int{
+    
+    var maxValue:Int = 0
+    
+    if numb1>numb2 && numb1>numb3{
+        maxValue = numb1
+    }
+    else if numb2>numb3 && numb2>numb1{
+        maxValue = numb2
+    }
+    else{
+        maxValue = numb3
+    }
+    return maxValue
 }
-else{
-    print("no")
-}
+var maxV = maxFinder(IntValue: 1, IntValue: 33, IntValue: 5)
+print(maxV)
+//---------------------------------------------------------------------------------------------------------------
+
